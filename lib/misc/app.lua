ad4c5d1734a8d4f7c04206f812131016ddd9f079
Catten Linger <cattenlinger@outlook.com>
2022-12-06 23:02:53 +0800

    Save codes

--- lib/misc/app.lua
+++ lib/misc/app.lua
@@ -3,6 +3,21 @@ local fs, library = require "misc.fs", require "misc.library"
 if not library "app" then -- App Lib
 --------------------------------------------------------------------------------------------------------------------------------
 local app_proto = app
+function app_proto:msg(...)
+    if self.quiet then return end
+    stderr(...)
+end
+
+function app_proto:info(...)
+    if self.quite or not self.verbose then return end
+    stderr(...)
+end
+
+function app_proto:dbg(...)
+    if self.quite or not self.debug then return end
+    stderr(...)
+end
+
 
 local features = table {}
 app_proto.features = table.protect({}, { __index = features:indexer() })
@@ -17,11 +32,22 @@ function app_proto:list_feature_files()
     return target
 end
 
+local function create_feature(name, proto)
+    if features[name] then error(fstring("Feature %q already exists", name)) end
+    local feature = table.protect({}, {
+        __index = table.indexer(proto),
+        __call = function(self, ...) self:entry(...) end,
+        __metatable = table.protect({ lib_name = name, is_instance = true, type = 'feature' })
+    })
+    features[name] = feature
+    return feature
+end
+
 local function create_feature_load_env(initial)
     return table.overlay(_G, initial)
 end
 
-local function load_feature_file(name, chain)
+local function load_feature(name, chain)
     local existed = features[name]
     if existed then return existed end
 
@@ -39,28 +65,20 @@ local function load_feature_file(name, chain)
     local filename = app.path.libraries + "/" + name + ".feature.lua"
     if not fs.is_file_exists(filename) then error("No such feature: " + name) end
     local global, mt = create_feature_load_env({
-        feature = function(name) load_feature_file(name, chain) end
+        load_feature = function(name) load_feature(name, chain) end;
+        feature = create_feature;
     })
-    local package, err = loadfile(filename, "bt", global)
-    if err then return nil, fstring("Could not load feature %q, error: %s", name, err) end
-    if not typeof(package) == 'feature' then error(fstring("Invalid definition of feature %q", name)) end
-    features[name] = package
-    return package
-end
+    local block, err = loadfile(filename, "bt", global)
+    if not block then return nil, fstring("Could not load feature %q, error: %s", name, err) end
+    
+    local _, feature, err = pcall(block)
+    if not feature then return nil, fstring("Cloud not evaluate feature %q, error: %s", name, err) end
 
-function app_proto:msg(...)
-    if self.quiet then return end
-    stderr(...)
-end
-
-function app_proto:info(...)
-    if self.quite or not self.verbose then return end
-    stderr(...)
-end
-
-function app_proto:dbg(...)
-    if self.quite or not self.debug then return end
-    stderr(...)
+    if (not typeof(feature) == 'feature') or (not feature.entry) then 
+        error(fstring("Invalid definition of feature %q. Created by 'create_feature' and entry function are required.", name)) 
+    end
+    features[name] = feature
+    return feature
 end
 
 local function print_information_and_exit()
@@ -83,9 +101,11 @@ function app_proto:main()
     app_proto.main = nil
     local subcommand = app.parameters.subcommand
     if #subcommand <= 0 then print_information_and_exit(); return end
-    local feature = subcommand[1]
-    local feature_args = pack(unpack(subcommand, 2))
 
+    local feature_name = subcommand[1]
+    local feature, err = load_feature(feature_name)
+    if not feature then os.exit(1, fstring("Could not load feature %q, error: %s", feature_name, err)) end
+    feature:entry(unpack(subcommand, 2))
 end
 
 app = library:create('app', app_proto)
